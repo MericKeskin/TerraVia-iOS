f947f8ede7589fb11af9bea8a25479d38cdfd156
Meric Keskin <mericyk@gmail.com>
2025-05-12 13:46:18 +0300

    Initialized Xcode project

---
+++ TerraVia/TerraVia/TerraViaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerraViaApp.swift
+//  TerraVia
+//
+//  Created by Meriç Keskin on 12.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TerraViaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
