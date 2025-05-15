8eb009da47d52ef5fb6a39932624df5c1db62a73
Meric Keskin <mericyk@gmail.com>
2025-05-15 17:13:49 +0300

    Settled app configuration, installed Firebase, initialized .gitignore
    Created new app environments and .xcconfig files for them.
    Added Firebase frameworks via CocoaPods.
    Updated .gitignore properly.

---
+++ TerraVia/TerraVia/Util/Config/AppConfig.swift
@@ -0,0 +1,28 @@
+//
+//  AppConfig.swift
+//  TerraVia
+//
+//  Created by Meriç Keskin on 14.05.2025.
+//
+
+import Foundation
+
+enum AppConfig {
+    case development
+    case staging
+    case production
+    
+    static let current: AppConfig = {
+#if DEV
+        .development
+#elseif STAG
+        .staging
+#else
+        .production
+#endif
+    }()
+    
+    var baseURL: String? {
+        Bundle.main.infoDictionary?["BaseURL"] as? String
+    }
+}
