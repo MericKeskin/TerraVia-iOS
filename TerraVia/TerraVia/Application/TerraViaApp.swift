1c794593b80393f943339184759d763e14d9635e
Meric Keskin <mericyk@gmail.com>
2025-05-15 17:56:30 +0300

    Created AppDelegate
    Connected AppDelegate to App Life Cycle for Firebase frameworks.

--- TerraVia/TerraVia/Application/TerraViaApp.swift
+++ TerraVia/TerraVia/Application/TerraViaApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct TerraViaApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
